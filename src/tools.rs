17e701a4462ba25e082e8f6fbcc9561ab68118bc
alsh <shpirkov@gmail.com>
2025-12-16 15:05:36 +0200

    Update tools.rs

--- src/tools.rs
+++ src/tools.rs
@@ -1,4 +1,3 @@
-
 struct Image {
     url: String,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -13,8 +12,67 @@ struct Image {
     description: Option<String>,
 }
 
-// делаем tool description
-struct ToolDescription {
-    name: String,
-    description: String,
+// делаем tool Descriptor
+#[derive(Deserialize, Serialize)]
+struct Descriptor;
+
+impl Tool for Descriptor {
+    const NAME: &'static str = "descriptor";
+    //type Error = MathError;
+    type Args = OperationArgs;
+    type Output = String;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        ToolDefinition {
+            name: "descriptor".to_string(),
+            description: "Desctip document by it ID".to_string(),
+            parameters: json!({
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "description": "Id of the document to describe"
+                    },
+                },
+                "required": ["id"]
+            }),
+        }
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        let result = args.id;
+        Ok(result)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct ImageFinder;
+
+impl Tool for ImageFinder {
+    const NAME: &'static str = "image_finder";
+    //type Error = MathError;
+    type Args = OperationArgs;
+    type Output = Image;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        ToolDefinition {
+            name: "image_finder".to_string(),
+            description: "Find images by query".to_string(),
+            parameters: json!({
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "description": "Id to search for images"
+                    },
+                },
+                "required": ["id"]
+            }),
+        }
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        let result = args.query;
+        Ok(result)
+    }
 }
