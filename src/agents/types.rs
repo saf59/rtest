0cc42a9a941f54d355dfd18c1829204541707813
alsh <shpirkov@gmail.com>
2026-02-07 13:54:10 +0200

    add tests for o && f

--- src/agents/types.rs
+++ src/agents/types.rs
@@ -67,6 +67,7 @@ pub struct WorkerRequest {
     pub context: WorkerContext,
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "chunk_type", content = "data")]
 pub enum WorkerParameters {
     GetObjectTree(TaskParameters),
     GetReportList {
@@ -143,6 +144,7 @@ pub enum OrchestratorDecision {
     },
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "chunk_type", content = "data")]
 pub enum WorkerType {
     GetObjectTree,
     GetReportList,
