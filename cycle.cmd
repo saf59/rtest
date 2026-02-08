f74be51d788406018cc9bbe88b240f7b5376c639
alsh <shpirkov@gmail.com>
2026-02-08 09:41:02 +0200

    fix test

--- cycle.cmd
+++ cycle.cmd
@@ -1,6 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set RUST_LOG=error
+set RUSTFLAGS=-A warnings
+
 :: Массив моделей для тестирования
 set MODELS=qwen3:14b qwen3-vl deepseek-r1:14b ministral-3:14b gemma3:12b minicpm-v:8b llava llama3.2-vision llava-llama3:latest functiongemma adelnazmy2002/Qwen3-VL-4B-Instruct:Q8_0
 
--- examples/warmup.rs
+++ examples/warmup.rs
@@ -1,7 +1,5 @@
-use rig::agent::stream_to_stdout;
-/// This example requires that you have the [`ollama`](https://ollama.com) server running locally.
+use rig::completion::Prompt;
 use rig::prelude::*;
-use rig::streaming::StreamingChat;
 use rig_test::helper::client;
 
 #[tokio::main]
@@ -10,13 +8,10 @@ async fn main() -> Result<(), anyhow::Error> {
     let model = std::env::var("OLLAMA_MODEL").unwrap_or("functiongemma:latest".to_string());
     let client = client(is_local);
     // Create agent with a single context prompt
-    let comedian_agent = client
+    let agent = client
         .agent(model)
         .preamble("You are a warmup module.")
         .build();
-    let messages = vec![  ];
-    let mut stream = comedian_agent.stream_chat("Warmup servr!", messages).await;
-    let response = stream_to_stdout(&mut stream).await.unwrap();
-    println!("Response: {:#?}", response.response());
+    let _ = agent.prompt("Warmup servr!").await;
     Ok(())
 }
